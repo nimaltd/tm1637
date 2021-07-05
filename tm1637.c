2c58bb9018a97bacc460a362243a320a16938df1
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2021-07-05 12:47:00 +0430

    Add files via upload

---
+++ tm1637.c
@@ -0,0 +1,145 @@
+
+#include "tm1637.h"
+#include "tm1637_config.h"
+#include <string.h>
+
+#if _TM1637_FREERTOS == 0
+#define tm1637_delay_ms(x)  HAL_Delay(x)
+#else
+#include "cmsis_os.h"
+#define tm1637_delay_ms(x)  osDelay(x)
+#endif
+
+#define TM1637_COMM1    0x40
+#define TM1637_COMM2    0xC0
+#define TM1637_COMM3    0x80
+//#######################################################################################################################
+void tm1637_delay_us(uint8_t delay)
+{
+  while (delay > 0)
+  {
+    delay--;
+    __nop();__nop();__nop();__nop();
+  }
+}
+//#######################################################################################################################
+void tm1637_start(tm1637_t *tm1637)
+{
+  HAL_GPIO_WritePin(tm1637->gpio_dat, tm1637->pin_dat, GPIO_PIN_RESET);
+  tm1637_delay_us(_TM1637_BIT_DELAY);
+}
+//#######################################################################################################################
+void tm1637_stop(tm1637_t *tm1637)
+{
+	HAL_GPIO_WritePin(tm1637->gpio_dat, tm1637->pin_dat, GPIO_PIN_RESET);
+	tm1637_delay_us(_TM1637_BIT_DELAY);
+	HAL_GPIO_WritePin(tm1637->gpio_clk, tm1637->pin_clk, GPIO_PIN_SET);
+	tm1637_delay_us(_TM1637_BIT_DELAY);
+	HAL_GPIO_WritePin(tm1637->gpio_dat, tm1637->pin_dat, GPIO_PIN_SET);
+	tm1637_delay_us(_TM1637_BIT_DELAY);
+}
+//#######################################################################################################################
+uint8_t tm1637_write_byte(tm1637_t *tm1637, uint8_t data)
+{
+  //  write 8 bit data
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    HAL_GPIO_WritePin(tm1637->gpio_clk, tm1637->pin_clk, GPIO_PIN_RESET);
+    tm1637_delay_us(_TM1637_BIT_DELAY);
+    if (data & 0x01)
+      HAL_GPIO_WritePin(tm1637->gpio_dat, tm1637->pin_dat, GPIO_PIN_SET);
+    else
+      HAL_GPIO_WritePin(tm1637->gpio_dat, tm1637->pin_dat, GPIO_PIN_RESET);
+    tm1637_delay_us(_TM1637_BIT_DELAY);
+    HAL_GPIO_WritePin(tm1637->gpio_clk, tm1637->pin_clk, GPIO_PIN_SET);
+    tm1637_delay_us(_TM1637_BIT_DELAY);
+    data = data >> 1;
+  }
+  // wait for acknowledge
+  HAL_GPIO_WritePin(tm1637->gpio_clk, tm1637->pin_clk, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(tm1637->gpio_dat, tm1637->pin_dat, GPIO_PIN_SET);
+  tm1637_delay_us(_TM1637_BIT_DELAY);
+  HAL_GPIO_WritePin(tm1637->gpio_clk, tm1637->pin_clk, GPIO_PIN_SET);
+  tm1637_delay_us(_TM1637_BIT_DELAY);
+  uint8_t ack = HAL_GPIO_ReadPin(tm1637->gpio_dat, tm1637->pin_dat);
+  if (ack == 0)
+    HAL_GPIO_WritePin(tm1637->gpio_dat, tm1637->pin_dat, GPIO_PIN_RESET);
+  tm1637_delay_us(_TM1637_BIT_DELAY);
+  HAL_GPIO_WritePin(tm1637->gpio_clk, tm1637->pin_clk, GPIO_PIN_RESET);
+  tm1637_delay_us(_TM1637_BIT_DELAY);
+  return ack;
+}
+//#######################################################################################################################
+void tm1637_lock(tm1637_t *tm1637)
+{
+  while (tm1637->lock == 1)
+    tm1637_delay_ms(1);
+  tm1637->lock = 1;  
+}
+//#######################################################################################################################
+void tm1637_unlock(tm1637_t *tm1637)
+{
+  tm1637->lock = 0;  
+}
+//#######################################################################################################################
+void tm1637_init(tm1637_t *tm1637, GPIO_TypeDef *gpio_clk, uint16_t pin_clk, GPIO_TypeDef *gpio_dat, uint16_t pin_dat)
+{
+  memset(tm1637, 0, sizeof(tm1637_t)); 
+  //  set max brightess
+  tm1637_brightness(tm1637, 7);  
+  tm1637_lock(tm1637);
+  //  init gpio
+  tm1637->gpio_clk = gpio_clk;
+  tm1637->pin_clk = pin_clk;
+  tm1637->gpio_dat = gpio_dat;
+  tm1637->pin_dat = pin_dat;
+  GPIO_InitTypeDef g = {0};
+  g.Mode = GPIO_MODE_OUTPUT_OD;
+  g.Pull = GPIO_NOPULL;
+  g.Speed = GPIO_SPEED_FREQ_HIGH;
+  g.Pin = pin_clk;
+  HAL_GPIO_Init(gpio_clk, &g);
+  g.Pin = pin_dat;
+  HAL_GPIO_Init(gpio_dat, &g);    
+  tm1637_unlock(tm1637);
+}
+//#######################################################################################################################
+void tm1637_write_segment(tm1637_t *tm1637, const uint8_t *segments, uint8_t length, uint8_t pos)
+{
+  tm1637_lock(tm1637);
+  // write COMM1
+	tm1637_start(tm1637);
+	tm1637_write_byte(tm1637, TM1637_COMM1);
+	tm1637_stop(tm1637);
+	// write COMM2 + first digit address
+	tm1637_start(tm1637);
+	tm1637_write_byte(tm1637, TM1637_COMM2 + (pos & 0x03));
+	// write the data bytes
+	for (uint8_t k=0; k < length; k++)
+	  tm1637_write_byte(tm1637, segments[k]);
+	tm1637_stop(tm1637);
+	// write COMM3 + brightness
+	tm1637_start(tm1637);
+	tm1637_write_byte(tm1637, TM1637_COMM3 + tm1637->brightness);
+	tm1637_stop(tm1637);
+  tm1637_unlock(tm1637);
+}
+//#######################################################################################################################
+void tm1637_brightness(tm1637_t *tm1637, uint8_t brightness_0_to_7)
+{
+  tm1637_lock(tm1637);
+  tm1637->brightness = (brightness_0_to_7 & 0x7) | 0x08;
+  tm1637_unlock(tm1637);
+}
+//#######################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
---
+++ tm1637_config.h
@@ -0,0 +1,7 @@
+#ifndef _TM1637_CONFIG_H_
+#define _TM1637_CONFIG_H_
+
+#define _TM1637_FREERTOS            0
+#define _TM1637_BIT_DELAY           20
+
+#endif
